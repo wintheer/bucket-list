b9bad10688bd93c9cb98ec2f2533e425a7cc183e
Frederik <frederikwinther@msn.com>
2021-03-02 22:07:13 +0100

    Added svg buttons on startpage

--- pages/index.vue
+++ pages/index.vue
@@ -10,10 +10,15 @@
         <div class="inspiration-images_left">
           <img class="image-fill" src="../assets/cliff-dive.jpg" alt="">
         </div>
-        
+        <button class="arrow left">
+          <ArrowButton/>
+        </button>
         <div class="inspiration-images_middle">
           <img class="image-fill" src="../assets/perform.jpg" alt="">
         </div>
+        <button class="arrow right">
+          <ArrowButton/>
+        </button>
         <div class="inspiration-images_right">
           <img class="image-fill" src="../assets/piano.jpg" alt="">
         </div>
@@ -24,8 +29,13 @@
 
 <script lang="ts">
 import Vue from "vue";
+import ArrowButton from "../assets/svgs/arrow.svg"
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: {
+    ArrowButton,
+  }
+});
 </script>
 
 <style>
@@ -48,12 +58,59 @@ h1 {
   justify-items: center;
 
   height: 50rem;
+}
 
+button {
+  background: transparent;
+  border: 0;
+  outline: 0;
+}
+
+path {
+  transition: all 250ms ease-in-out;
+}
+
+.arrow {
+  cursor: pointer;
+  width: 70px;
+  height: 90px;
+  align-self: center;
+  z-index: 2;
+}
+
+.left{
+  grid-area: left;
+  justify-self: end;
+}
+
+.right {
+  grid-area: right;
+  justify-self: start;
+  transform: rotate(180deg);
+}
+
+.arrow:hover path {
+  stroke-width: 6;
+}
+
+.arrow:active path {
+  stroke-width: 8;
+  transition: all 200ms ease-in-out;
 }
 
 @media screen and (max-width: 992px) {
   .content {
     grid-template-columns: 100vw;
+  }
+
+  .left {
+    grid-area: left;
+    justify-self: start;
+  }
+
+  .right {
+    grid-area: left;
+    justify-self: end;
   }
 }
 
@@ -93,6 +150,5 @@ h1 {
 
 .image-fill {
   height: 100%;
-  
 }
 </style>
